ccab75a1306220d2a7aa2ad807c8f9ace5217de6
Jagrat2106 <guptajagrat020106@gmail.com>
2026-04-19 04:39:36 +0530

    Update pqc_key_exchange.py

--- pqc_key_exchange.py
+++ pqc_key_exchange.py
@@ -1,6 +1,21 @@
 import os
 import hashlib
 
+KEY_PATH = "files/shared_key.bin"
+
 def generate_shared_secret():
-    # simulate PQC shared secret using secure randomness
-    return hashlib.sha256(os.urandom(32)).digest()
+    # generate random key (PQC-like)
+    random_bytes = os.urandom(32)
+    shared_secret = hashlib.sha256(random_bytes).digest()
+
+    # save key to file
+    with open(KEY_PATH, "wb") as f:
+        f.write(shared_secret)
+
+    return shared_secret
+
+
+def load_shared_secret():
+    # load key from file
+    with open(KEY_PATH, "rb") as f:
+        return f.read()
